e088bfe7bbad61093e3123fc86d7a8d4023f619b
Loic Fuentes <fuentes.loic@gmail.com>
2024-09-28 15:05:51 -0400

    feat: create loader component

---
+++ app/components/loader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center items-center flex-col h-full">
+    <UIcon name="i-heroicons-arrow-path" class="h-12 w-12 animate-spin mb-4" />
+    <p class="text-lg font-medium text-gray-500 dark:text-gray-300 ml-4">
+      Loading data, please wait...
+    </p>
+  </div>
+</template>
